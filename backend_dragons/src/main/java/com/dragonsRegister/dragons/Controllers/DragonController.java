97181f4406f5a23e5486ee6d02e34a3292734657
Gabriel Monteiro <gabrieldealmeidamonteiro@gmail.com>
2023-01-23 16:29:10 -0300

    terminando método do DragonController para adicionar dados no banco de dados

--- backend_dragons/src/main/java/com/dragonsRegister/dragons/Controllers/DragonController.java
+++ backend_dragons/src/main/java/com/dragonsRegister/dragons/Controllers/DragonController.java
@@ -1,12 +1,14 @@
 package com.dragonsRegister.dragons.Controllers;
 
 import com.dragonsRegister.dragons.DatabaseClasses.Dragon;
+import com.dragonsRegister.dragons.DatabaseClasses.DragonRepository;
 import com.dragonsRegister.dragons.DatabaseClasses.DragonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DragonController {
@@ -14,9 +16,9 @@ public class DragonController {
     @Autowired
     private DragonService dragonService;
 
-    @PostMapping("/newdragon")
-    public HttpStatus RegistrarDragon(@RequestBody Dragon dragon) {
+    @PostMapping(path="/newdragon", produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=*/*", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String RegistrarDragon(@RequestBody Dragon dragon) {
         dragonService.salvar(dragon);
-        return dragon.Ok();
+        return "Registro Concluído";
     }
 }
