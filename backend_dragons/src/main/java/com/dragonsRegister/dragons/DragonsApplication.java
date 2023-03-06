e9f754496ed7517c25085f4178b0dfd3d756d5bc
Gabriel Monteiro <gabrieldealmeidamonteiro@gmail.com>
2023-03-06 19:04:04 -0300

    arrumando conexão com MongoDB

--- backend_dragons/src/main/java/com/dragonsRegister/dragons/DragonsApplication.java
+++ backend_dragons/src/main/java/com/dragonsRegister/dragons/DragonsApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {
-		MongoAutoConfiguration.class,
-		MongoDataAutoConfiguration.class
-})
+@SpringBootApplication
 @RestController
 public class DragonsApplication {
 
