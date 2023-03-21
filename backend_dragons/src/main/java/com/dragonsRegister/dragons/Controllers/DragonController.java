135c0eddbf7c412643ec2c63867f26145d97dbe5
Gabriel Monteiro <gabrieldealmeidamonteiro@gmail.com>
2023-03-21 11:15:01 -0300

    Mudança nos URI da API

--- backend_dragons/src/main/java/com/dragonsRegister/dragons/Controllers/DragonController.java
+++ backend_dragons/src/main/java/com/dragonsRegister/dragons/Controllers/DragonController.java
@@ -20,7 +20,7 @@ public class DragonController {
     @Autowired
     private DragonService dragonService;
 
-    @PostMapping(path="/newdragon", produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=*/*", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path="/dragon/create", produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=*/*", consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody HttpStatus RegistrarDragon(@RequestBody Dragon dragon) {
         int id = dragonService.checarId();
         dragon.setId(id);
@@ -28,19 +28,19 @@ public class DragonController {
         return HttpStatus.OK;
     }
 
-    @GetMapping("/receivedragon")
+    @GetMapping("/dragon/read")
     public ResponseEntity<List<Dragon>> getAllDragons() {
         List<Dragon> dragons = dragonService.getDragons();
         return new ResponseEntity<>(dragons, HttpStatus.OK);
     }
 
-    @PostMapping(path="/deletedragon", produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=*/*", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path="/dragon/delete", produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=*/*", consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody HttpStatus deletarDragon(@RequestBody Dragon dragon) {
         dragonService.deletar(dragon);
         return HttpStatus.OK;
     }
 
-    @PostMapping(path="/updatedragon", produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=*/*", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path="/dragon/update", produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=*/*", consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody HttpStatus atualizarDragon(@RequestBody Dragon dragon) {
         dragon = dragonService.checaEAtualizaNomeETipoSeNecessario(dragon);
         dragonService.salvar(dragon);
