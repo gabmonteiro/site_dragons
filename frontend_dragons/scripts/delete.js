5eaabfc839180bb65c00de21ecf6edc422717df4
Gabriel Monteiro <gabrieldealmeidamonteiro@gmail.com>
2023-03-23 11:39:14 -0300

    mudando pagina de delete

--- frontend_dragons/scripts/delete.js
+++
@@ -1,9 +0,0 @@
-function sendId() {
-    //pegando valor do ID
-    let id = document.getElementById("id").value;
-
-    //enviando form em JSON pro backend por HTTP POST
-    fetch("https://api-dragons-production.up.railway.app/dragons/"+id, {
-        method: 'DELETE'})
-        .then(alert("deletado"))
-}
--- frontend_dragons/scripts/list.js
+++ frontend_dragons/scripts/list.js
@@ -1,17 +1,39 @@
 function getDragons() {
+  //fazedno http get e ordenando array de objetos
   fetch("https://api-dragons-production.up.railway.app/dragons").then(
     async function (response) {
       var listaJson = await response.json();
       console.log(listaJson);
+      listaJson.sort(function(a, b) {
+        if(a.id < b.id) {
+          return -1;
+        } else {
+          return true;
+        }
+      })
+
+      //colocando irformações no html
       let nomes = "";
       let contagem=0;
       listaJson.forEach((element) => {
-        nomes += `<p><strong>${element.id} Nome:</strong> ${element.nome} <br> <strong>Tipo:</strong> ${element.tipo}</p>`;
+        nomes += `<tr><td>${element.id}</td><td>${element.nome}</td><td>${element.tipo}</td><td><input type="button" value="Del" id="deleteButton" onclick="sendIdToDelete(${element.id})"></input></td></tr>`;
         contagem++;
       });
-      document.querySelector("#info").innerHTML += nomes;
+      document.querySelector("#tabela").innerHTML += nomes;
     }
   );
 }
-
 getDragons();
+
+
+function sendIdToDelete(id) {
+  //enviando form em JSON pro backend por HTTP POST
+  fetch("https://api-dragons-production.up.railway.app/dragons/"+id, {
+      method: 'DELETE'})
+      .then(
+        alert("deletado"),
+        window.location.reload(true)
+      )
+}
+
+
