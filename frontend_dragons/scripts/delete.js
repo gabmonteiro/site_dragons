65ffc63a8b6661bf2b896e78bfe4bc859ab71ab1
Gabriel Monteiro <gabrieldealmeidamonteiro@gmail.com>
2023-03-23 10:11:48 -0300

    Arrumando o 0

--- frontend_dragons/scripts/delete.js
+++ frontend_dragons/scripts/delete.js
@@ -3,7 +3,7 @@ function sendId() {
     let id = document.getElementById("id").value;
 
     //enviando form em JSON pro backend por HTTP POST
-    fetch("https://api-dragons-production.up.railway.app/dragons/0", {
+    fetch("https://api-dragons-production.up.railway.app/dragons/"+id, {
         method: 'DELETE'})
         .then(alert("deletado"))
 }
