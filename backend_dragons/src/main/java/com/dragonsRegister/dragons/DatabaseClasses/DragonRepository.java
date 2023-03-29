95c2d33598292add279e7e630927913fe8654e52
Gabriel Monteiro <gabrieldealmeidamonteiro@gmail.com>
2023-03-29 16:08:44 -0300

    Dividindo site da API

--- backend_dragons/src/main/java/com/dragonsRegister/dragons/DatabaseClasses/DragonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dragonsRegister.dragons.DatabaseClasses;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DragonRepository extends MongoRepository<Dragon, String> {
-}
